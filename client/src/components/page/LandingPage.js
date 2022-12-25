1224450b7db00b2ecbf432ba3def856dc2e2ebda
Zaiza2000 <69431042+Zaiza2000@users.noreply.github.com>
2022-12-25 16:26:49 +0700

    Update Landing page

--- client/src/components/page/LandingPage.js
+++ client/src/components/page/LandingPage.js
@@ -2,11 +2,10 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import '../../App.css';
 
-export default class LandingPage extends Component {
-    render() {
-        return (
-            <div className="App">
-                <section
+export default function LandingPage() {
+    return (
+        <div className="App">
+            {/* <section
                     className="relative bg-[url(https://lh5.googleusercontent.com/p/AF1QipM7r8QDnx3kNkqAodgKB9x9l6CEiYpueL1TKsP3=w1080-k-no)] bg-cover bg-center bg-no-repeat"
                 >
                     <div
@@ -47,8 +46,122 @@ export default class LandingPage extends Component {
                         </div>
                     </div>
             </div>
-                </section >
-            </div >
-        );
-    }
+                </section > */}
+            <section className="pt-20 pb-10 lg:pt-[120px] lg:pb-20">
+                <div className="container mx-auto">
+                    <div className="-mx-4 flex flex-wrap justify-center">
+                        <div className="w-full px-4">
+                            <div className="mx-auto mb-[60px] max-w-[510px] text-center lg:mb-20">
+                                <span className="text-primary mb-2 block text-lg font-semibold">
+                                    ยินดีต้อนรับ
+                                </span>
+                                <h2
+                                    className="text-dark mb-4 text-3xl font-bold sm:text-4xl md:text-[40px]"
+                                >
+                                    ปรีชาพานิชย์
+                                </h2>
+                                <p className="text-body-color text-base">
+                                    อะไหล่ Honda แท้
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="-mx-4 flex flex-wrap">
+                        <div className="w-full px-4 md:w-1/2 lg:w-1/3">
+                            <div className="mx-auto mb-10 max-w-[370px]">
+                                <div className="mb-8 overflow-hidden rounded">
+                                    <img
+                                        src="https://cdn.tailgrids.com/2.0/image/application/images/blogs/blog-01/image-01.jpg"
+                                        alt="image"
+                                        className="w-full"
+                                    />
+                                </div>
+                                <div>
+                                    <span
+                                        className="bg-primary mb-5 inline-block rounded py-1 px-4 text-center text-xs font-semibold leading-loose text-white"
+                                    >
+                                        Dec 22, 2023
+                                    </span>
+                                    <h3>
+                                        <a
+                                            href="javascript:void(0)"
+                                            className="text-dark hover:text-primary mb-4 inline-block text-xl font-semibold sm:text-2xl lg:text-xl xl:text-2xl"
+                                        >
+                                            Meet AutoManage, the best AI management tools
+                                        </a>
+                                    </h3>
+                                    <p className="text-body-color text-base">
+                                        Lorem Ipsum is simply dummy text of the printing and typesetting
+                                        industry.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="w-full px-4 md:w-1/2 lg:w-1/3">
+                            <div className="mx-auto mb-10 max-w-[370px]">
+                                <div className="mb-8 overflow-hidden rounded">
+                                    <img
+                                        src="https://cdn.tailgrids.com/2.0/image/application/images/blogs/blog-01/image-02.jpg"
+                                        alt="image"
+                                        className="w-full"
+                                    />
+                                </div>
+                                <div>
+                                    <span
+                                        className="bg-primary mb-5 inline-block rounded py-1 px-4 text-center text-xs font-semibold leading-loose text-white"
+                                    >
+                                        Mar 15, 2023
+                                    </span>
+                                    <h3>
+                                        <a
+                                            href=""
+                                            className="text-dark hover:text-primary mb-4 inline-block text-xl font-semibold sm:text-2xl lg:text-xl xl:text-2xl"
+                                        >
+                                            How to earn more money as a wellness coach
+                                        </a>
+                                    </h3>
+                                    <p className="text-body-color text-base">
+                                        Lorem Ipsum is simply dummy text of the printing and typesetting
+                                        industry.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="w-full px-4 md:w-1/2 lg:w-1/3">
+                            <div className="mx-auto mb-10 max-w-[370px]">
+                                <div className="mb-8 overflow-hidden rounded">
+                                    <img
+                                        src="https://cdn.tailgrids.com/2.0/image/application/images/blogs/blog-01/image-03.jpg"
+                                        alt="image"
+                                        className="w-full"
+                                    />
+                                </div>
+                                <div>
+                                    <span
+                                        className="bg-primary mb-5 inline-block rounded py-1 px-4 text-center text-xs font-semibold leading-loose text-white"
+                                    >
+                                        Jan 05, 2023
+                                    </span>
+                                    <h3>
+                                        <a
+                                            href="javascript:void(0)"
+                                            className="text-dark hover:text-primary mb-4 inline-block text-xl font-semibold sm:text-2xl lg:text-xl xl:text-2xl"
+                                        >
+                                            The no-fuss guide to upselling and cross selling
+                                        </a>
+                                    </h3>
+                                    <p className="text-body-color text-base">
+                                        Lorem Ipsum is simply dummy text of the printing and typesetting
+                                        industry.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        </div >
+    );
 }
+
