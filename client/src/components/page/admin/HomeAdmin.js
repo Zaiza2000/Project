a15745a5fe056c4c503ad9deb9775a7efb67e508
Zaiza2000 <69431042+Zaiza2000@users.noreply.github.com>
2023-01-19 00:24:52 +0700

    Update HomeAdmin.js

--- client/src/components/page/admin/HomeAdmin.js
+++ client/src/components/page/admin/HomeAdmin.js
@@ -1,15 +1,52 @@
 import NavbarLogin from "../../layouts/NavbarLogin";
-import MenubarAdmin from "../../layouts/MenubarAdmin"; 
+import MenubarAdmin from "../../layouts/MenubarAdmin";
 import AdminProductCard from "../../card/AdminProductCard";
+import React, { useState, useEffect } from "react";
 
-export default function HomeAdmin(){
-    return(
+const initialstate = {
+    product_name: "",
+    product_cost: "",
+    product_sale: "",
+    product_photo: "",
+    product_detail: "",
+    product_num: "",
+    category_id: "",
+};
+export default function HomeAdmin() {
+    const [value, setValue] = useState({
+        product_name: "",
+        product_cost: "",
+        product_sale: "",
+        product_photo: "",
+        product_detail: "",
+        product_num: "",
+        category_id: "",
+    });
+    // useEffect(() => {
+    //     loadData();
+    //   }, []);
+    
+    //   const loadData = () => {
+    //     listCategory()
+    //       .then((res) => {
+    //         setCategory(res.data);
+    //       })
+    //       .catch((err) => {
+    //         console.log(err);
+    //       });
+    //   };
+    return (
         <div>
-            <NavbarLogin/>
+            <NavbarLogin />
             <h1>HomeAdmin</h1>
-            <MenubarAdmin/>
-            <AdminProductCard/>
+            <MenubarAdmin />
+            {/* {
+                product_photo.map((item) => (
+                    <AdminProductCard />
+                ))
+            } */}
+
         </div>
-        
+
     )
 }
