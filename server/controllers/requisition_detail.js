553c98860e02227f39abb2b009f78f3f9277d850
mesinee09 <mesinee.yo@kkumail.com>
2023-03-13 23:33:38 +0700

    requisition_detail (server)

---
+++ server/controllers/requisition_detail.js
@@ -0,0 +1,25 @@
+const Requisition_Detail = require("../models/Requisition_Detail");
+const {Op} = require("sequelize");
+
+exports.listRequisition = async (req, res) => {
+  try {
+    const requisition = await Requisition_Detail.findAll();
+    res.send(requisition);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("==Server Error (listRequisition) ==");
+  }
+};
+
+exports.getRequisition = async (req, res) => {
+    try {
+        console.log(req.params);
+      const requisition = await Requisition_Detail.findAll({
+         where: { RID :  "RID000001" },
+      });
+      res.json(requisition);
+    } catch (error) {
+      console.log(error);
+      res.status(500).send("==Server Error (getRequisition) ==");
+    }
+  };
