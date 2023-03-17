362c9f7e4021565b8ccd24d26b471e83892e2295
Zaiza2000 <69431042+Zaiza2000@users.noreply.github.com>
2023-03-17 18:10:25 +0700

    Update UI CheckOut.js
    Not Finished Yet

--- client/src/components/page/CheckOut.js
+++ client/src/components/page/CheckOut.js
@@ -216,179 +216,156 @@ export default function CheckOut() {
       <div className="flex space-x-10 m-20">
         {/*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>   Shipping   <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/}
 
-        <div id="checkout" className="flex-1 w-32 ">
+        <div id="checkout" className="flex-1 w-64 ">
           ที่อยู่สำหรับจัดส่งสินค้า
-          <div className="flex ">
-            <div>
-              {/* <div className="form-check">
-                <input
-                  className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 my-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                  type="radio"
-                  name="flexRadioDefault"
-                  id="flexRadioDefault10"
-                  checked
-                ></input>
-                <label
-                  className="form-check-label inline-block text-gray-800"
-                  for="flexRadioDefault1"
-                >
-                  ใช้ที่อยู่เดียวกับที่อยู่ของสมาชิก
-                </label>
-              </div> */}
-              <div className="form-check">
-                {/* <input
-                  className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 my-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                  type="radio"
-                  name="flexRadioDefault"
-                  id="flexRadioDefault20"
-                ></input> */}
-                <label
-                  className="form-check-label inline-block text-gray-800"
-                  for="flexRadioDefault2"
-                >
-                  {/* ใช้ที่อยู่ใหม่ */}
-                  <form className="mt-6" onSubmit={handleSubmit}>
-                    <div className="mb-2">
-                      <label
-                        for="text"
-                        className="block text-sm font-semibold text-gray-800"
-                      >
-                        ชื่อ
-                      </label>
-                      <input
-                        type="text"
-                        name="shipping_firstname"
-                        placeholder="ชื่อ"
-                        onChange={(e) => handleChange(e)}
-                        className="block w-full px-4 py-2 mt-2  bg-white border rounded-md focus:border-red-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                      />
-                      <input
-                        type="text"
-                        name="shipping_lastname"
-                        placeholder="นามสกุล"
-                        onChange={(e) => handleChange(e)}
-                        className="block w-full px-4 py-2 mt-2  bg-white border rounded-md focus:border-red-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                      />
-                    </div>
-                    <div className="mb-2">
-                      <label className="block text-sm font-semibold text-gray-800">
-                        ที่อยู่
-                      </label>
-                      <textarea
-                        type="text"
-                        name="shipping_address"
-                        placeholder="เลขที่ 85/8 หมู่ 13 ต.ในเมือง "
-                        onChange={(e) => handleChange(e)}
-                        className="block w-full px-2 py-2 mt-2  bg-white border rounded-md focus:border-red-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                      />
-                    </div>
-                    <div className="flex flex-wrap -mx-3 mb-2">
-                      <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                        <label className="block text-sm font-semibold text-gray-800">
-                          จังหวัด
-                        </label>
-                        <select
-                          className="block w-64 px-4 py-2 mt-2 bg-white border rounded-md focus:border-red-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                          name="shipping_province"
-                          onChange={(e) => onChangeProvince_shipping(e)}
-                        >
-                          {province_shipping.map((item, index) => (
-                            <option key={index} value={item.id}>
-                              {item.name_th}
-                            </option>
-                          ))}
-                        </select>
-                      </div>
-                    </div>
-                    <div className="flex flex-wrap -mx-3 mb-2">
-                      <div className="w-64 md:w-1/2 px-3 mb-6 md:mb-0">
-                        <label
-                          className="block text-sm font-semibold text-gray-800"
-                          for=""
-                        >
-                          อำเภอ
-                        </label>
-                        <select
-                          className="block w-64 px-4 py-2 mt-2 bg-white border rounded-md focus:border-red-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                          name="shipping_district"
-                          onChange={(e) => onChangeDistrict_shipping(e)}
-                        >
-                          {district_shipping.map((item, index) => (
-                            <option key={index} value={item.id}>
-                              {item.name_th}
-                            </option>
-                          ))}
-                        </select>
-                      </div>
-                    </div>
 
-                    <div className="flex flex-wrap -mx-3 mb-2">
-                      <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                        <label
-                          className="block text-sm font-semibold text-gray-800"
-                          for=""
-                        >
-                          ตำบล
-                        </label>
-                        <select
-                          className="block w-64 px-4 py-2 mt-2 bg-white border rounded-md focus:border-red-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                          name="shipping_sub_district"
-                          onChange={(e) => onChangeSubDistrict_shipping(e)}
-                        >
-                          {sub_district_shipping.map((item, index) => (
-                            <option key={index} value={item.id}>
-                              {item.name_th}
-                            </option>
-                          ))}
-                        </select>
-                      </div>
-                    </div>
-                    <div className="flex flex-wrap -mx-3 mb-2">
-                      <div className="w-64 md:w-1/2 px-3 mb-6 md:mb-0">
-                        <label
-                          className="block text-sm font-semibold text-gray-800"
-                          for=""
-                        >
-                          รหัสไปรษณีย์
-                        </label>
-                        <input
-                          className="block w-64 px-4 py-2 mt-2 bg-white border rounded-md focus:border-red-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                          id="grid-zip"
-                          type="text"
-                          name="shipping_zipcode"
-                          value={value.zipcode_shipping}
-                          onChange={(e) => handleChange(e)}
-                        />
-                      </div>
-                    </div>
-                    <div className="mb-2">
-                      <label
-                        for="telephone"
-                        className="block text-sm font-semibold text-gray-800"
-                      >
-                        เบอร์โทรศัพท์
-                      </label>
-                      <input
-                        type="text"
-                        name="shipping_tel"
-                        placeholder="098-888-8888"
-                        onChange={(e) => handleChange(e)}
-                        className="block w-full px-4 py-2 mt-2  bg-white border rounded-md focus:border-red-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                      />
-                    </div>
-                    <div className="mt-6">
-                      <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-red-700 rounded-md hover:bg-red-600 focus:outline-none focus:bg-purple-600">
-                        ยื่นยันข้อมูล
-                      </button>
-                    </div>
-                  </form>
-                </label>
-              </div>
-            </div>
+          <div className="form-check">
+
+            <label
+              className="form-check text-gray-800"
+              for="flexRadioDefault2"
+            >
+              {/* ใช้ที่อยู่ใหม่ */}
+              <form className="mt-6" onSubmit={handleSubmit}>
+                <div className="mb-2">
+                  <label
+                    for="text"
+                    className="block text-sm font-semibold text-gray-800"
+                  >
+                    ชื่อ
+                  </label>
+                  <input
+                    type="text"
+                    name="shipping_firstname"
+                    placeholder="ชื่อ"
+                    onChange={(e) => handleChange(e)}
+                    className="block  px-4 py-2 mt-2  bg-white border rounded-md focus:border-red-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                  />
+                  <input
+                    type="text"
+                    name="shipping_lastname"
+                    placeholder="นามสกุล"
+                    onChange={(e) => handleChange(e)}
+                    className="block px-4 py-2 mt-2  bg-white border rounded-md focus:border-red-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                  />
+                </div>
+                <div className="mb-2">
+                  <label className="block text-sm font-semibold text-gray-800">
+                    ที่อยู่
+                  </label>
+                  <textarea
+                    type="text"
+                    name="shipping_address"
+                    placeholder="เลขที่ 85/8 หมู่ 13 ต.ในเมือง "
+                    onChange={(e) => handleChange(e)}
+                    className="block  px-2 py-2 mt-2  bg-white border rounded-md focus:border-red-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                  />
+                </div>
+                <div className="-mx-3 mb-2">
+                  <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                    <label className="block text-sm font-semibold text-gray-800">
+                      จังหวัด
+                    </label>
+                    <select
+                      className="block w-64 px-4 py-2 mt-2 bg-white border rounded-md focus:border-red-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                      name="shipping_province"
+                      onChange={(e) => onChangeProvince_shipping(e)}
+                    >
+                      {province_shipping.map((item, index) => (
+                        <option key={index} value={item.id}>
+                          {item.name_th}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                </div>
+                <div className=" -mx-3 mb-2">
+                  <div className="w-64 md:w-1/2 px-3 mb-6 md:mb-0">
+                    <label
+                      className="block text-sm font-semibold text-gray-800"
+                      for=""
+                    >
+                      อำเภอ
+                    </label>
+                    <select
+                      className="block w-64 px-4 py-2 mt-2 bg-white border rounded-md focus:border-red-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                      name="shipping_district"
+                      onChange={(e) => onChangeDistrict_shipping(e)}
+                    >
+                      {district_shipping.map((item, index) => (
+                        <option key={index} value={item.id}>
+                          {item.name_th}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                </div>
+
+                <div className="-mx-3 mb-2">
+                  <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                    <label
+                      className="block text-sm font-semibold text-gray-800"
+                      for=""
+                    >
+                      ตำบล
+                    </label>
+                    <select
+                      className="block w-64 px-4 py-2 mt-2 bg-white border rounded-md focus:border-red-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                      name="shipping_sub_district"
+                      onChange={(e) => onChangeSubDistrict_shipping(e)}
+                    >
+                      {sub_district_shipping.map((item, index) => (
+                        <option key={index} value={item.id}>
+                          {item.name_th}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                </div>
+                <div className="-mx-3 mb-2">
+                  <div className="w-64 md:w-1/2 px-3 mb-6 md:mb-0">
+                    <label
+                      className="block text-sm font-semibold text-gray-800"
+                      for=""
+                    >
+                      รหัสไปรษณีย์
+                    </label>
+                    <input
+                      className="block w-64 px-4 py-2 mt-2 bg-white border rounded-md focus:border-red-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                      id="grid-zip"
+                      type="text"
+                      name="shipping_zipcode"
+                      value={value.zipcode_shipping}
+                      onChange={(e) => handleChange(e)}
+                    />
+                  </div>
+                </div>
+                <div className="mb-2">
+                  <label
+                    for="telephone"
+                    className="block text-sm font-semibold text-gray-800"
+                  >
+                    เบอร์โทรศัพท์
+                  </label>
+                  <input
+                    type="text"
+                    name="shipping_tel"
+                    placeholder="098-888-8888"
+                    onChange={(e) => handleChange(e)}
+                    className="block  px-4 py-2 mt-2  bg-white border rounded-md focus:border-red-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                  />
+                </div>
+                <div className="mt-6">
+                  <button className="w-3/4 px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-red-700 rounded-md hover:bg-red-600 focus:outline-none focus:bg-purple-600">
+                    ยืนยันข้อมูล
+                  </button>
+                </div>
+              </form>
+            </label>
           </div>
         </div>
         {/*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>   Billing   <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<,<<<<<<<<<<<<<<<<<<*/}
-        <div id="checkout" className="flex-1 w-32 ">
+        <div id="checkout" className="flex-1 w-64 ">
           ที่อยู่ในการออกใบเสร็จ
           <div className="flex ">
             <div className="form-check ">
@@ -400,7 +377,7 @@ export default function CheckOut() {
               </label>
             </div>
             <div className="form-check">
-           
+
               <label
                 className="form-check-label inline-block text-gray-800"
                 for="flexRadioDefault2"
@@ -538,7 +515,7 @@ export default function CheckOut() {
                       name="billing_tel"
                       placeholder="098-888-8888"
                       onChange={(e) => handleChange(e)}
-                      className="block w-full px-4 py-2 mt-2  bg-white border rounded-md focus:border-red-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                      className="block  px-4 py-2 mt-2  bg-white border rounded-md focus:border-red-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
                     />
                   </div>
                 </form>
@@ -547,7 +524,7 @@ export default function CheckOut() {
           </div>
         </div>
 
-        <form className="flex-1 w-32 ">
+        <form className="flex-1 w-64 ">
           <div id="checkout" className="">
             การชำระเงิน
             <div className="form-check">
@@ -574,7 +551,7 @@ export default function CheckOut() {
                 <lable htmlFor="customfile">{filename}</lable>
               </div>
               <div>
-                <div className="mb-2">
+                <div className="">
                   <label
                     for="text"
                     className="block text-sm font-semibold text-gray-800"
@@ -597,6 +574,12 @@ export default function CheckOut() {
 
         <div id="checkout" className="flex-1 w-32 ">
           ยืนยันการสั่งซื้อ
+          <label
+            for="text"
+            className="block text-sm font-semibold text-gray-800"
+          >
+            หมายเลขประจำตัวผู้เสียภาษี
+          </label>
         </div>
       </div>
     </div>
