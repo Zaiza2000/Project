248392d9f51263aa73c7332d93b01245a7ed61e9
Zaiza2000 <69431042+Zaiza2000@users.noreply.github.com>
2023-03-19 01:17:12 +0700

    Update Shop.js

--- client/src/components/page/member/Shop.js
+++ client/src/components/page/member/Shop.js
@@ -68,14 +68,14 @@ export default function Shop() {
     let inState = [...categorySelect]
 
     let findCheck = inState.indexOf(inCheck)
-    if(findCheck === -1) {
+    if (findCheck === -1) {
       inState.push(inCheck)
-    }else{
+    } else {
       inState.splice(findCheck, 1)
     }
     setCategorySelect(inState)
-    fetchDataFilter({category:inState})
-    if(inState.length < 1){
+    fetchDataFilter({ category: inState })
+    if (inState.length < 1) {
       loadData()
 
     }
@@ -102,30 +102,20 @@ export default function Shop() {
               max={1000}
               onChange={(e) => setPrice(e.target.value)}
             />
-            <hr />
             <h4 className="text-2xl text-left pt-20 pb-5">ค้นหาตามหมวดหมู่สินค้า</h4>
-            <div className="items-center mb-4">
-              {category.map((item,index) =>
-                <Checkbox
+
+            {category.map((item, index) =>
+              <div className="flex items-center mb-4">
+                <input id="default-checkbox" type="checkbox"
                   onChange={handleCheck}
                   value={item.category_id}
-                  className="w-4 h-4 m-10  bg-gray-100 border-gray-300 rounded focus:ring-blue-500  focus:ring-2">
-                  <div className="ml-2 text-xl">
-                    {item.category_name}
-                  </div>
-                </Checkbox>
-              )}
+                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500  focus:ring-2  "></input>
+                <label for="default-checkbox" className="ml-2 text-xl">{item.category_name}</label>
+              </div>
+            )}
 
-            </div>
-            {/* <div className="flex items-center mb-4">
-              <input id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500  focus:ring-2  "></input>
-              <label for="default-checkbox" className="ml-2 text-xl">H2C</label>
-            </div> */}
           </div>
         </div>
-        {/* <div className="row-span-2 col-span-2 ">
-
-        </div> */}
 
         <div className="col-span-2">
           {loading ? (
