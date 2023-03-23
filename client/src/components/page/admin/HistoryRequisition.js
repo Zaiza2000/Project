34c866b8412e0e6418db8d4da055556732a175e2
Zaiza2000 <69431042+Zaiza2000@users.noreply.github.com>
2023-03-23 22:18:46 +0700

    Update HistoryRequisition.js

--- client/src/components/page/admin/HistoryRequisition.js
+++ client/src/components/page/admin/HistoryRequisition.js
@@ -1,6 +1,6 @@
 
 import React, { useEffect, useState } from "react";
-import { PDFDownloadLink,PDFPreview  } from '@react-pdf/renderer';
+import { PDFDownloadLink, PDFPreview } from '@react-pdf/renderer';
 
 //function
 import {
@@ -24,8 +24,8 @@ export default function HistoryRequisition() {
     function get_has_run_status() {
       return !localStorage.getItem('hasRun');
     }
-    
-    const loadData = async() => {
+
+    const loadData = async () => {
       await listRequisitionByRID().then((res) => {
         setRequisition(res.data);
         console.log("res.data => ", res.data);
@@ -36,22 +36,22 @@ export default function HistoryRequisition() {
       });
     };
 
-    const loadListRIDData = async(item) => {
-     
+    const loadListRIDData = async (item) => {
+
       await getRequisition(item.RID).then((res) => {
         item.listOfRID = res.data;
         localStorage.setItem(item.RID, JSON.stringify(item.listOfRID))
-        
+
       });
     }
 
-    async function get_loadData(){
+    async function get_loadData() {
       if (get_has_run_status()) {
         localStorage.setItem('hasRun', true);
         await loadData();
-        setTimeout(function(){
+        setTimeout(function () {
           window.location.reload();
-       }, 3000); // 3 seconds
+        }, 3000); // 3 seconds
       } else {
         await loadData();
       }
@@ -66,89 +66,99 @@ export default function HistoryRequisition() {
   function show_log() {
     var m = new Date();
     var dateString =
-        m.getFullYear() + "/" +
-        ("0" + (m.getMonth()+1)).slice(-2) + "/" +
-        ("0" + m.getDate()).slice(-2) + " " +
-        ("0" + m.getHours()).slice(-2) + ":" +
-        ("0" + m.getMinutes()).slice(-2) + ":" +
-        ("0" + m.getSeconds()).slice(-2);
-    
+      m.getFullYear() + "/" +
+      ("0" + (m.getMonth() + 1)).slice(-2) + "/" +
+      ("0" + m.getDate()).slice(-2) + " " +
+      ("0" + m.getHours()).slice(-2) + ":" +
+      ("0" + m.getMinutes()).slice(-2) + ":" +
+      ("0" + m.getSeconds()).slice(-2);
+
     console.log("\n".repeat(5) + "======".repeat(5) + `\t${dateString}\t` + "======".repeat(5) + "\n".repeat(5));
   }
 
   const tableHead = (
-    <thead class="text-xs text-black uppercase bg-gray-50 dark:bg-red-700 dark:text-white">
-              <tr>
-                <th scope="col" class="px-6 py-3">
-                  RID
-                </th>
-                <th scope="col" class="px-6 py-3">
-                  รหัสสินค้า
-                </th>
-                <th scope="col" class="px-6 py-3">
-                  สินค้า
-                </th>
-                <th scope="col" class="px-6 py-3">
-                  รายละเอียด
-                </th>
-                <th scope="col" class="px-6 py-3">
-                  จำนวน
-                </th>
-                <th scope="col" class="px-6 py-3">
-                  ราคาต้นทุน
-                </th>
-            
-              </tr>
-            </thead>
+    <thead className="text-xs text-black uppercase bg-blue-400 ">
+      <tr>
+        <th scope="col" class="px-6 py-3">
+          RID
+        </th>
+        <th scope="col" class="px-6 py-3">
+          รหัสสินค้า
+        </th>
+        <th scope="col" class="px-6 py-3">
+          สินค้า
+        </th>
+        <th scope="col" class="px-6 py-3">
+          รายละเอียด
+        </th>
+        <th scope="col" class="px-6 py-3">
+          จำนวน
+        </th>
+        <th scope="col" class="px-6 py-3">
+          ราคาต้นทุน
+        </th>
+
+      </tr>
+    </thead>
   )
 
   const tableData = (item) => {
-    return JSON.parse(localStorage.getItem(item.RID)).map( (inner_item) => (
-    <tbody>
-      <tr class="bg-gray-200 border-b dark:bg-g00 dark:border-gray-700">
-        <th
-          scope="row"
-          class="px-6 py-4 font-medium text-black whitespace-nowrap dark:text-black"
-        >
-          {inner_item.RID}
-        </th>
-        <td class="px-6 py-4">{inner_item.product_id}</td>
-        <td class="px-6 py-4">{inner_item.product_name}</td>
-        <td class="px-6 py-4">{inner_item.product_detail}</td>
-        <td class="px-6 py-4">{inner_item.quantity}</td>
-        <td class="px-6 py-4">{inner_item.price}</td>
-        
-      </tr>
-    </tbody>
-  ))}
+    return JSON.parse(localStorage.getItem(item.RID)).map((inner_item) => (
+      <tbody>
+        <tr className="bg-gray-100 border-b ">
+          <th
+            scope="row"
+            className="px-6 py-4 font-medium text-black whitespace-nowrap "
+          >
+            {inner_item.RID}
+          </th>
+          <td className="px-6 py-4">{inner_item.product_id}</td>
+          <td className="px-6 py-4">{inner_item.product_name}</td>
+          <td className="px-6 py-4">{inner_item.product_detail}</td>
+          <td className="px-6 py-4">{inner_item.quantity}</td>
+          <td className="px-6 py-4">{inner_item.price}</td>
+
+        </tr>
+      </tbody>
+    ))
+  }
 
   return (
-    <div>
-      <h1>HistoryRequisition</h1>
+    <div className="flex flex-row">
       <MenubarAdmin />
-     
-      
-      {requisition.map((item, index) => (
-        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-          <table class="w-full text-sm text-left text-black dark:text-black">
-            {tableHead}
-            {tableData(item)}
-          </table>
-          <br></br>
-        <div>
-          
-            <PDFDownloadLink 
-            document={
-              <RequisitionPDF requisition_pdf={item} localStorage_items={JSON.parse(localStorage.getItem(item.RID))}/>
-            }
-            fileName="RequisitionPDF.pdf">
-              PDF Download
-            </PDFDownloadLink>
+
+      <div className="pr-16">
+        <h3 className="text-4xl p-10 font-bold text-purple-600">ประวัติการเบิกสินค้า</h3>
+
+        {requisition.map((item, index) => (
+          <div className="">
+            <table className="w-full text-sm text-left text-black ">
+              {tableHead}
+              {tableData(item)}
+            </table>
+            <br></br>
+            <div >
+              <PDFDownloadLink
+
+                className="bg-gray-200 hover:bg-blue-300 py-3 px-2"
+                document={
+                  <RequisitionPDF requisition_pdf={item} localStorage_items={JSON.parse(localStorage.getItem(item.RID))}
+                  />
+
+                }
+                fileName="RequisitionPDF.pdf">
+                <button >
+                  PDF Download
+                </button>
+
+              </PDFDownloadLink>
+
+            </div>
+            <br></br>
+
           </div>
-          <br></br>
-      
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
